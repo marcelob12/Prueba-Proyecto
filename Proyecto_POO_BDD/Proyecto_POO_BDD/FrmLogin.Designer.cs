ae94f7c7b3bcda33e79f82550379512ec51f3ffd
Marcelo <marcelobran11@gmail.com>
2021-06-21 22:50:06 -0600

    Modify FormLogin

--- Proyecto_POO_BDD/Proyecto_POO_BDD/FrmLogin.Designer.cs
+++ Proyecto_POO_BDD/Proyecto_POO_BDD/FrmLogin.Designer.cs
@@ -29,16 +29,21 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmLogin));
             this.lblUser = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
             this.txtUser = new System.Windows.Forms.TextBox();
             this.txtPassword = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
             this.btnStartSesion = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize) (this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // lblUser
             // 
+            this.lblUser.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
+            this.lblUser.ForeColor = System.Drawing.Color.MidnightBlue;
             this.lblUser.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
             this.lblUser.Location = new System.Drawing.Point(221, 211);
             this.lblUser.Name = "lblUser";
@@ -49,11 +54,14 @@
             // 
             // label1
             // 
-            this.label1.Location = new System.Drawing.Point(261, 255);
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
+            this.label1.ForeColor = System.Drawing.Color.MidnightBlue;
+            this.label1.Location = new System.Drawing.Point(195, 255);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(100, 23);
+            this.label1.Size = new System.Drawing.Size(126, 23);
             this.label1.TabIndex = 1;
             this.label1.Text = "Constraseña:";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             // 
             // txtUser
             // 
@@ -72,29 +80,47 @@
             // 
             // label2
             // 
-            this.label2.Location = new System.Drawing.Point(311, 131);
+            this.label2.Font = new System.Drawing.Font("Microsoft YaHei", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
+            this.label2.ForeColor = System.Drawing.Color.MidnightBlue;
+            this.label2.Location = new System.Drawing.Point(221, 73);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(188, 45);
+            this.label2.Size = new System.Drawing.Size(376, 45);
             this.label2.TabIndex = 4;
             this.label2.Text = "Sistema Vacunación  COVID-19\r\n";
-            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // btnStartSesion
             // 
-            this.btnStartSesion.Location = new System.Drawing.Point(337, 316);
+            this.btnStartSesion.BackColor = System.Drawing.Color.LightBlue;
+            this.btnStartSesion.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnStartSesion.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
+            this.btnStartSesion.ForeColor = System.Drawing.Color.MidnightBlue;
+            this.btnStartSesion.Location = new System.Drawing.Point(327, 314);
             this.btnStartSesion.Name = "btnStartSesion";
-            this.btnStartSesion.Size = new System.Drawing.Size(151, 47);
+            this.btnStartSesion.Size = new System.Drawing.Size(172, 27);
             this.btnStartSesion.TabIndex = 5;
             this.btnStartSesion.Text = "Iniciar sesion";
-            this.btnStartSesion.UseVisualStyleBackColor = true;
+            this.btnStartSesion.UseVisualStyleBackColor = false;
             this.btnStartSesion.Click += new System.EventHandler(this.btnStartSesion_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image) (resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(354, 102);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(109, 93);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
             // 
             // FrmLogin
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSize = true;
+            this.BackColor = System.Drawing.Color.MintCream;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.btnStartSesion);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.txtPassword);
@@ -104,9 +130,12 @@
             this.MaximizeBox = false;
             this.Name = "FrmLogin";
             this.Text = "Login Manager";
+            ((System.ComponentModel.ISupportInitialize) (this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
         }
+
+        private System.Windows.Forms.PictureBox pictureBox1;
 
         private System.Windows.Forms.Button btnStartSesion;
 
